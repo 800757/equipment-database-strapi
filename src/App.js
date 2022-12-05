d2239c310521c15be43b905d731a16cd25fe3a6c
Diana <lihacevadiana06@gmail.com>
2022-12-05 03:53:48 +0300

    Deployed Website

--- src/App.js
+++ src/App.js
@@ -1,25 +1,100 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import data from './data.json';
 
-function App() {
+//function App() {
+
+  //const [contacts, setContacts] = useState(data)
+  const EditableTable = () => {
+    const [equipmentData, setEquipmentData] = useState(data)
+  
+    const onChangeInput = (e, equipmentId) => {
+      const { name, value } = e.target
+  
+      const editData = equipmentData.map((item) =>
+        item.equipmentId === equipmentId && name ? { ...item, [name]: value } : item
+      )
+  
+      setEquipmentData(editData)
+    }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App-container">
+      <h1 className="title">ReactJS Editable Table</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Equipment Type</th>
+            <th>Equipment Name</th>
+            <th>Employee Name</th>
+            <th>Position</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        <tbody>
+        {equipmentData.map(({ equipmentId, equipmentType, equipmentName, employeeName, position, email }) => (
+            <tr key={equipmentId}>
+              <td className="id">
+                <input
+                  name="equipmentId"
+                  value={equipmentId}
+                  type="text"
+                  onChange={(e) => onChangeInput(e, equipmentId)}
+                  placeholder=" "
+                />
+              </td>
+              <td className="type">
+                <input
+                  name="equipmentType"
+                  value={equipmentType}
+                  type="text"
+                  onChange={(e) => onChangeInput(e, equipmentId)}
+                  placeholder="Type"
+                />
+              </td>
+              <td>
+                <input
+                  name="equipmentName"
+                  value={equipmentName}
+                  type="text"
+                  onChange={(e) => onChangeInput(e, equipmentId)}
+                  placeholder="Type Equipment Name"
+                />
+              </td>
+              <td>
+                <input
+                  name="employeeName"
+                  value={employeeName}
+                  type="text"
+                  onChange={(e) => onChangeInput(e, equipmentId)}
+                  placeholder="Type Employee Name"
+                />
+              </td>
+              <td>
+                <input
+                  name="position"
+                  type="text"
+                  value={position}
+                  onChange={(e) => onChangeInput(e, equipmentId)}
+                  placeholder="Type Position"
+                />
+              </td>
+              <td>
+                <input
+                  name="email"
+                  value={email}
+                  type="text"
+                  onChange={(e) => onChangeInput(e, equipmentId)}
+                  placeholder="Type Email"
+                />
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
-
-export default App;
+export default EditableTable;
+//export default App;
